3504d45c979f88985ef23889a96d429050b3905b
MMMMMMoSky <q94354906@gmail.com>
2019-02-23 21:36:06 +0800

    Chapter 1 - 3.5 : WinFormApp Edition 1

---
+++ Product-WinFormApp/Product-WinFormApp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Product_WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LHS = new System.Windows.Forms.TextBox();
+            this.RHS = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LHS
+            // 
+            this.LHS.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LHS.Location = new System.Drawing.Point(25, 82);
+            this.LHS.Name = "LHS";
+            this.LHS.Size = new System.Drawing.Size(214, 64);
+            this.LHS.TabIndex = 0;
+            this.LHS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LHS.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // RHS
+            // 
+            this.RHS.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RHS.Location = new System.Drawing.Point(277, 82);
+            this.RHS.Name = "RHS";
+            this.RHS.Size = new System.Drawing.Size(214, 64);
+            this.RHS.TabIndex = 1;
+            this.RHS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.RHS.TextChanged += new System.EventHandler(this.RHS_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(527, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 199);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RHS);
+            this.Controls.Add(this.LHS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LHS;
+        private System.Windows.Forms.TextBox RHS;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Product-WinFormApp/Product-WinFormApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product_WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        private int a, b;
+        private bool ok_a, ok_b;
+
+        private void RHS_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                b = Convert.ToInt32(RHS.Text);
+                ok_b = true;
+            }
+            catch (System.FormatException)
+            {
+                ok_b = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ok_a && ok_b)
+                button1.Text = Convert.ToString(a * b);
+            else
+                button1.Text = "Invalid";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            a = 0;
+            b = 0;
+            ok_a = false;
+            ok_b = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToInt32(LHS.Text);
+                ok_a = true;
+            }
+            catch (System.FormatException)
+            {
+                ok_a = false;
+            }
+        }
+    }
+}
