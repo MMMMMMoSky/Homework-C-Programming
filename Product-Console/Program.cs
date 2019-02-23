b2e0c8f964166556b2bf5072b8299b2112ebb3cd
MMMMMMoSky <q94354906@gmail.com>
2019-02-23 18:50:06 +0800

    Chapter 1 3.4

---
+++ Product-Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Product_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lhs, rhs;
+            Console.WriteLine("------A x B Problem------");
+
+            Console.Write("Input an integer: ");
+            lhs = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input another integer: ");
+            rhs = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("There product: ");
+            Console.WriteLine(Convert.ToString(lhs * rhs));
+        }
+    }
+}
