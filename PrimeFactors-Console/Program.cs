9dc0b685edb02fd483429b71ca29f5fd7b63b260
MMMMMMoSky <q94354906@gmail.com>
2019-03-04 09:25:11 +0800

    Homework2: P78 6

---
+++ PrimeFactors-Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PrimeFactors_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an integer: ");
+
+            long x;
+
+            // Make sure it's valid
+            try
+            {
+                x = Convert.ToInt64(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: please input an integer");
+                return;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: please input an integer in range of [{0}, {1}]",
+                        long.MinValue, long.MaxValue);
+                return;
+            }
+
+            // Algorithm begins
+            Console.Write("Prime factors: ");
+            for (long i = 2; Math.Abs(x) > 1; i++)
+            {
+                if (x % i == 0)
+                {
+                    Console.Write(Convert.ToString(i) + " ");
+                }
+                while (x % i == 0)
+                {
+                    x /= i;
+                }
+            }
+        }
+    }
+}
