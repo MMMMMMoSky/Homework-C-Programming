8c38120d5c47e3fc076417e926078f26c059031e
MMMMMMoSky <q94354906@gmail.com>
2019-03-25 10:05:11 +0800

    namespace changed

--- Hw5_OrderService/Customer.cs
+++ Hw5_OrderService/Customer.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ordertest
+namespace OrderService
 {
-
     /// <summary>
     /// Customer the man who orders goods.
     /// </summary>
--- Hw5_OrderService/OrderService.cs
+++ Hw5_OrderService/OrderService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ordertest
+namespace OrderService
 {
     /// <summary>
     /// OrderService:provide ordering service,
