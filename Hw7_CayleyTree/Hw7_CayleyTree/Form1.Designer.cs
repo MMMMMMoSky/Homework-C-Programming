cec0020a8d2c5ae434b2bff0120561da8b552030
MMMMMMoSky <q94354906@gmail.com>
2019-04-15 18:49:36 +0800

    Homework7: 第五章课后15题 改进例5-31“

---
+++ Hw7_CayleyTree/Hw7_CayleyTree/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Hw7_CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.draw = new System.Windows.Forms.Button();
+            this.vScrollBar_per2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar_per1 = new System.Windows.Forms.VScrollBar();
+            this.color = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // draw
+            // 
+            this.draw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.draw.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.draw.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.draw.Location = new System.Drawing.Point(1171, 684);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(137, 75);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "draw";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // vScrollBar_per2
+            // 
+            this.vScrollBar_per2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.vScrollBar_per2.Location = new System.Drawing.Point(1288, 312);
+            this.vScrollBar_per2.Maximum = 70;
+            this.vScrollBar_per2.Name = "vScrollBar_per2";
+            this.vScrollBar_per2.Size = new System.Drawing.Size(20, 322);
+            this.vScrollBar_per2.TabIndex = 1;
+            this.vScrollBar_per2.Value = 40;
+            this.vScrollBar_per2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_per2_Scroll);
+            // 
+            // vScrollBar_per1
+            // 
+            this.vScrollBar_per1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.vScrollBar_per1.Location = new System.Drawing.Point(1171, 312);
+            this.vScrollBar_per1.Maximum = 70;
+            this.vScrollBar_per1.Name = "vScrollBar_per1";
+            this.vScrollBar_per1.Size = new System.Drawing.Size(20, 322);
+            this.vScrollBar_per1.TabIndex = 2;
+            this.vScrollBar_per1.Value = 50;
+            this.vScrollBar_per1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_per1_Scroll);
+            // 
+            // color
+            // 
+            this.color.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.color.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.color.FormattingEnabled = true;
+            this.color.ItemHeight = 36;
+            this.color.Items.AddRange(new object[] {
+            "red",
+            "yellow",
+            "blue",
+            "black",
+            "pink"});
+            this.color.Location = new System.Drawing.Point(1171, 62);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(137, 184);
+            this.color.TabIndex = 3;
+            this.color.SelectedIndexChanged += new System.EventHandler(this.color_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 800);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.vScrollBar_per1);
+            this.Controls.Add(this.vScrollBar_per2);
+            this.Controls.Add(this.draw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.VScrollBar vScrollBar_per2;
+        private System.Windows.Forms.VScrollBar vScrollBar_per1;
+        private System.Windows.Forms.ListBox color;
+    }
+}
+
---
+++ Hw7_CayleyTree/Hw7_CayleyTree/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hw7_CayleyTree
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graphics;
+        private Pen pen = Pens.Blue;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.4;
+        double per2 = 0.5;
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            if (graphics == null)
+            {
+                graphics = this.CreateGraphics();
+            }
+            graphics.Clear(Color.White);
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+
+        void drawCayleyTree(int n, double x0, double y0, double len, double th)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            double x1 = x0 + len * Math.Cos(th);
+            double y1 = y0 + len * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * len, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * len, th - th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void vScrollBar_per1_Scroll(object sender, ScrollEventArgs e)
+        {
+            per1 = (double)this.vScrollBar_per1.Value / 100;
+        }
+
+        private void vScrollBar_per2_Scroll(object sender, ScrollEventArgs e)
+        {
+            per2 = (double)this.vScrollBar_per2.Value / 100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void color_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (this.color.SelectedIndex)
+            {
+                case 0:
+                    pen = Pens.Red;
+                    break;
+                case 1:
+                    pen = Pens.Yellow;
+                    break;
+                case 2:
+                    pen = Pens.Blue;
+                    break;
+                case 3:
+                    pen = Pens.Black;
+                    break;
+                case 4:
+                    pen = Pens.Pink;
+                    break;
+                default:
+                    pen = Pens.Blue;
+                    break;
+            }
+        }
+    }
+}
