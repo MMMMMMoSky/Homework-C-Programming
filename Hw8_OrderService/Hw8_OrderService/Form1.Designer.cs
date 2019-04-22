aa4e0eb572524618ca1e2b65e17ab2cce63741cf
MMMMMMoSky <q94354906@gmail.com>
2019-04-22 15:36:10 +0800

    Homework8: WinForm + OrderService 初步实现了查询和数据绑定演示

---
+++ Hw8_OrderService/Hw8_OrderService/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Hw8_OrderService
+{
+    partial class OrderServiceForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.QueryButton = new System.Windows.Forms.Button();
+            this.QueryBy = new System.Windows.Forms.ComboBox();
+            this.QueryResult = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalAmountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.QueryContent = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QueryResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.QueryContent);
+            this.panel1.Controls.Add(this.ExportButton);
+            this.panel1.Controls.Add(this.ImportButton);
+            this.panel1.Controls.Add(this.QueryButton);
+            this.panel1.Controls.Add(this.QueryBy);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1744, 160);
+            this.panel1.TabIndex = 0;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ExportButton.Location = new System.Drawing.Point(282, 41);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(140, 74);
+            this.ExportButton.TabIndex = 3;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImportButton.Location = new System.Drawing.Point(86, 41);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(134, 74);
+            this.ImportButton.TabIndex = 2;
+            this.ImportButton.Text = "Import";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            // 
+            // QueryButton
+            // 
+            this.QueryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.QueryButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.QueryButton.Location = new System.Drawing.Point(824, 41);
+            this.QueryButton.Name = "QueryButton";
+            this.QueryButton.Size = new System.Drawing.Size(172, 74);
+            this.QueryButton.TabIndex = 1;
+            this.QueryButton.Text = "Query";
+            this.QueryButton.UseVisualStyleBackColor = true;
+            this.QueryButton.Click += new System.EventHandler(this.QueryButton_Click);
+            // 
+            // QueryBy
+            // 
+            this.QueryBy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.QueryBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.QueryBy.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.QueryBy.FormattingEnabled = true;
+            this.QueryBy.Location = new System.Drawing.Point(1038, 57);
+            this.QueryBy.Name = "QueryBy";
+            this.QueryBy.Size = new System.Drawing.Size(328, 44);
+            this.QueryBy.TabIndex = 0;
+            this.QueryBy.SelectedIndexChanged += new System.EventHandler(this.QueryBy_SelectedIndexChanged);
+            // 
+            // QueryResult
+            // 
+            this.QueryResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.QueryResult.AutoGenerateColumns = false;
+            this.QueryResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.QueryResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.customerDataGridViewTextBoxColumn,
+            this.totalAmountDataGridViewTextBoxColumn});
+            this.QueryResult.DataSource = this.orderBindingSource;
+            this.QueryResult.Location = new System.Drawing.Point(0, 166);
+            this.QueryResult.Name = "QueryResult";
+            this.QueryResult.RowTemplate.Height = 37;
+            this.QueryResult.Size = new System.Drawing.Size(1744, 802);
+            this.QueryResult.TabIndex = 1;
+            this.QueryResult.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.QueryResult_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // customerDataGridViewTextBoxColumn
+            // 
+            this.customerDataGridViewTextBoxColumn.DataPropertyName = "Customer";
+            this.customerDataGridViewTextBoxColumn.HeaderText = "Customer";
+            this.customerDataGridViewTextBoxColumn.Name = "customerDataGridViewTextBoxColumn";
+            // 
+            // totalAmountDataGridViewTextBoxColumn
+            // 
+            this.totalAmountDataGridViewTextBoxColumn.DataPropertyName = "TotalAmount";
+            this.totalAmountDataGridViewTextBoxColumn.HeaderText = "TotalAmount";
+            this.totalAmountDataGridViewTextBoxColumn.Name = "totalAmountDataGridViewTextBoxColumn";
+            this.totalAmountDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // orderBindingSource
+            // 
+            this.orderBindingSource.DataSource = typeof(ordertest.Order);
+            // 
+            // QueryContent
+            // 
+            this.QueryContent.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.QueryContent.Location = new System.Drawing.Point(1426, 57);
+            this.QueryContent.Name = "QueryContent";
+            this.QueryContent.Size = new System.Drawing.Size(224, 43);
+            this.QueryContent.TabIndex = 4;
+            // 
+            // OrderServiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1744, 1054);
+            this.Controls.Add(this.QueryResult);
+            this.Controls.Add(this.panel1);
+            this.Name = "OrderServiceForm";
+            this.Text = "Order Service";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QueryResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox QueryBy;
+        private System.Windows.Forms.Button QueryButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.DataGridView QueryResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalAmountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource orderBindingSource;
+        private System.Windows.Forms.TextBox QueryContent;
+    }
+}
+
---
+++ Hw8_OrderService/Hw8_OrderService/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ordertest;
+
+namespace Hw8_OrderService
+{
+    public partial class OrderServiceForm : System.Windows.Forms.Form
+    {
+
+        private string[] queryType = { "ALL", "By ID",
+            "By Customer Name",
+            "By Goods Name",
+            "By Total Amount" };
+
+        private OrderService orderService;
+
+        public OrderServiceForm()
+        {
+            InitializeComponent();
+            this.QueryBy.DataSource = queryType;
+            this.QueryBy.SelectedIndex = 0;
+
+            Customer customer1 = new Customer(1, "Customer1");
+            Customer customer2 = new Customer(2, "Customer2");
+
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Goods eggs = new Goods(2, "eggs", 4.99f);
+            Goods apple = new Goods(3, "apple", 5.59f);
+
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(apple, 8));
+            order1.AddDetails(new OrderDetail(eggs, 10));
+            order1.AddDetails(new OrderDetail(milk, 10));
+
+            Order order2 = new Order(2, customer2);
+            order2.AddDetails(new OrderDetail(eggs, 10));
+            order2.AddDetails(new OrderDetail(milk, 10));
+
+            Order order3 = new Order(3, customer2);
+            order3.AddDetails(new OrderDetail(milk, 100));
+
+            orderService = new OrderService();
+            orderService.AddOrder(order1);
+            orderService.AddOrder(order2);
+            orderService.AddOrder(order3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QueryBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QueryButton_Click(object sender, EventArgs e)
+        {
+            List<Order> res;
+            switch (this.QueryBy.SelectedIndex)
+            {
+                case 0: res = orderService.QueryAll(); break;
+                case 1:
+                    res = new List<Order>();
+                    res.Add(orderService.GetById(Convert.ToInt32(this.QueryContent.Text)));
+                    break;
+                case 2:
+                    res = orderService.QueryByCustomerName(this.QueryContent.Text);
+                    break;
+                case 3:
+                    res = orderService.QueryByGoodsName(this.QueryContent.Text);
+                    break;
+                case 4:
+                    res = orderService.QueryByTotalAmount((float)Convert.ToDouble(this.QueryContent.Text));
+                    break;
+                default: res = new List<Order>(); break;
+            }
+            this.orderBindingSource.DataSource = res;
+        }
+
+        private void QueryResult_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
